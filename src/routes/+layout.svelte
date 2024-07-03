b51b9dba6f984c0a0f188eb227586fd3eeeef947
Nashtech <anuj.tomar@nashtechglobal.com>
2024-07-03 16:26:54 +0530

    Svelte authentication with cookies

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script>
+	import './styles.css';
+    import Navigation from '$lib/components/Navigation.svelte';
+	import { goto } from '$app/navigation';
+
+    export let data;
+    
+	/**
+	 * @param {{ detail: { signout: any; }; }} event
+	 */
+	async function handleSignOut(event) {
+		const status = event.detail.signout;
+        await goto(`/?signout=${status}`);
+	}
+</script>
+
+<div class="app">
+	<Navigation user={data.user} on:signout={handleSignOut}/>
+
+    <div class="container mx-auto mt-8 antialiased">
+        <slot />
+    </div>
+</div>
---
+++ src/routes/sign-up/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import SignUpForm from '$lib/components/SignUpForm.svelte';
+	/**
+	 * @type {any}
+	 */
+	let error;
+</script>
+
+<h1 class="text-2xl font-semibold text-center">Sign Up</h1>
+{#if error}
+	<p class="mt-3 text-red-500 text-center font-semibold">{error}</p>
+{/if}
+<SignUpForm class="max-w-xl mx-auto mt-8" />
